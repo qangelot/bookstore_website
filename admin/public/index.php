73d7787d29a99aa3b6143a326b409eabfb657055
Julien Simonet <j.mons54@gmail.com>
2019-11-03 22:57:51 +0100

    fix link

--- admin/public/index.php
+++ admin/public/index.php
@@ -19,11 +19,8 @@
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="./">Livres</a>
-          </li>
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="bookDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <a class="nav-link dropdown-toggle" href="./" id="bookDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               Livres
             </a>
             <div class="dropdown-menu" aria-labelledby="bookDropdown">
